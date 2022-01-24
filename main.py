57cb5260d0a821df5319fc24d3b4cac7f5a26f5b
John Adams Jr <john.c.adams32@gmail.com>
2022-01-23 22:06:59 -0700

    game working. bug in display function

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 import turtle
+from turtle import Turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("Guess the States!")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+words = Turtle()
+words.hideturtle()
 
-answer_state = screen.textinput(title="Guess a State", prompt="What is another states name?")
 # TODO: Display states name after user guesses it.
-# TODO: Implement game over
-# TODO: Pull data from the csv
 
-user_guess_score = 0
-state_in_us = csv[0]
-answer_state = screen.textinput(title="Guess a State", prompt="What is another states name?")
+answer_state = screen.textinput(title="Guess a State", prompt="What a states name?")
+user_score = 0
+state_data = pandas.read_csv("50_states.csv")
+state_in_us = state_data.state.to_list()
+print(state_in_us)
 
 
-def display_text():
+def display_text(answer_state):
+    state = state_data[state_data.state == answer_state.title()]
+    state_name = state.state
+    state_location = (int(state.x), int(state.y))
+    words.penup()
+    words.goto(state_location)
+    words.write(f"{state_name.values}", font=("Courier", 9, "normal"))
     pass
 
 
-while user_guess_score < 50:
-    for state in state_in_us:
-        if answer_state.title() == state:
-            answer_state = screen.textinput(title=f"{user_guess_score}/50States Correct", prompt="What is another "
-                                                                                                 "states name?")
-            display_text()
-            user_guess_score += 1
+gameplay = True
 
-    if user_guess_score == 50:
-        turtle.write("Game Over! You Guessed all the states!")
+while gameplay:
+
+    if answer_state.title() in state_in_us:
+        display_text(answer_state)
+        user_score += 1
+        answer_state = screen.textinput(title=f"{user_score}/50States Correct", prompt="What is another "
+                                                                                       "states name?")
+    else:
+        words.penup()
+        words.goto(-150, 0)
+        words.write("Game Over. You Lose!", font=("Courier", 24, "normal"))
+        gameplay = False
+
+    if user_score == 50:
+        words.write("Game Over! You Guessed all the states!", font=("Courier", 24, "normal"))
 
 screen.exitonclick()
