26e6b6682ed6ac7022c11f4c69af9265af2e43cb
John Adams Jr <john.c.adams32@gmail.com>
2022-01-23 22:54:30 -0700

    added study aspect, needs comments

--- main.py
+++ main.py
@@ -10,13 +10,10 @@ turtle.shape(image)
 words = Turtle()
 words.hideturtle()
 
-# TODO: Display states name after user guesses it.
-
 answer_state = screen.textinput(title="Guess a State", prompt="What a states name?").title()
 user_score = 0
 state_data = pandas.read_csv("50_states.csv")
 state_in_us = state_data.state.to_list()
-print(state_in_us)
 
 
 def display_text(answer_state):
@@ -30,10 +27,21 @@ def display_text(answer_state):
 
 
 gameplay = True
+guessed_states = []
 
 while gameplay:
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in state_in_us:
+            if state not in guessed_states:
+                missing_states.append(state)
+        things_to_study = pandas.DataFrame(missing_states)
+        things_to_study.to_csv("things-to-study")
+        break
+
     if answer_state in state_in_us:
+        guessed_states.append(answer_state)
         display_text(answer_state)
         user_score += 1
         answer_state = screen.textinput(title=f"{user_score}/50States Correct", prompt="What is another "
@@ -47,4 +55,3 @@ while gameplay:
     if user_score == 50:
         words.write("Game Over! You Guessed all the states!", font=("Courier", 24, "normal"))
 
-screen.exitonclick()
