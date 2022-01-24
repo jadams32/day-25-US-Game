b18cffa8b4270cc6e8ca4231ab20d4fd6d33ae77
John Adams Jr <john.c.adams32@gmail.com>
2022-01-23 19:59:02 -0700

    Basic features added. Todo added

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import turtle
+
+screen = turtle.Screen()
+screen.title("Guess the States!")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_state = screen.textinput(title="Guess a State", prompt="What is another states name?")
+
+screen.exitonclick()
+# TODO: Display states name after user guesses it.
+# TODO: Implement counter
+# TODO: Implement game over
+# TODO: Pull data from the csv
+
