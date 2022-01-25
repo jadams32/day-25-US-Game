162696be9f63c67cf53bb438d5721c899a76252b
John Adams Jr <john.c.adams32@gmail.com>
2022-01-24 21:55:01 -0700

    Refactored, comments added.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# For day 25 I create a states guessing game. Play through the game guessing states until you can no longer remember
+# anymore. Once you're finished type 'exit' to receive a new file with the list of states that you missed.
+
+# Import files needed
 import turtle
 from turtle import Turtle
 import pandas
 
+# Initialize the screen and create the background of states
 screen = turtle.Screen()
 screen.title("Guess the States!")
 image = "blank_states_img.gif"
@@ -10,13 +15,16 @@ turtle.shape(image)
 words = Turtle()
 words.hideturtle()
 
-answer_state = screen.textinput(title="Guess a State", prompt="What a states name?").title()
-user_score = 0
+# Store states in a list
 state_data = pandas.read_csv("50_states.csv")
 state_in_us = state_data.state.to_list()
 
+# Prompt the user to guess a state name
+answer_state = screen.textinput(title="Guess a State", prompt="What a states name?").title()
+
 
 def display_text(answer_state):
+    """Displays the states name over the state after user guesses correctly."""
     state = state_data[state_data.state == answer_state]
     state_name = state.state
     state_location = (int(state.x), int(state.y))
@@ -26,16 +34,16 @@ def display_text(answer_state):
     pass
 
 
+# Variables used
+user_score = 0
 gameplay = True
 guessed_states = []
 
+# Main Game Loop
 while gameplay:
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in state_in_us:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in state_in_us if state not in guessed_states]
         things_to_study = pandas.DataFrame(missing_states)
         things_to_study.to_csv("things-to-study")
         break
@@ -54,4 +62,3 @@ while gameplay:
 
     if user_score == 50:
         words.write("Game Over! You Guessed all the states!", font=("Courier", 24, "normal"))
-
